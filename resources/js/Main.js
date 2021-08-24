45be8deaf1d782d313a6b196769385d83adb7629
Juliano Seus <julianoseusdev@outlook.com>
2021-08-23 22:13:39 -0300

    Trabalho na Estrutura Geral, no Menu e no Titulo da Pagina

---
+++ resources/js/Main.js
@@ -0,0 +1,21 @@
+import General from './General.js';
+import Home from './Home.js';
+
+export default class Main { 
+
+static main = function() {
+    const pageInformation = new General();
+
+    pageInformation.initializePage();
+
+    if (pageInformation.route) {
+        if (pageInformation.route == "Home") {
+            Home();
+        }
+    }
+}
+
+
+
+}
+
