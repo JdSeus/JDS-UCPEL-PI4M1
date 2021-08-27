ba252875bb7d9e3a63c97455b136ab78ed58575c
Juliano Seus <julianoseusdev@outlook.com>
2021-08-26 21:33:37 -0300

    Trabalho na Galeria de Posts

--- resources/js/Home.js
+++ resources/js/Home.js
@@ -1,12 +1,17 @@
 export default function () { 
-    $('.carousel-container').slick({
+    $('section.carousel  .carousel-container').slick({
         fade: true,
         arrows: true,
         prevArrow: $('section.carousel .carousel-arrows .left'),
-        nextArrow: $('section.carousel .carousel-arrows .right'),
-
-        
+        nextArrow: $('section.carousel .carousel-arrows .right'), 
     });
-    console.log( $('section.carousel .carousel-arrows'));
+
+    $('section.posts-gallery-carousel .posts-gallery-carousel-container').slick({
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: $('section.posts-gallery-carousel .posts-gallery-carousel-arrows .left'),
+        nextArrow: $('section.posts-gallery-carousel .posts-gallery-carousel-arrows .right'), 
+    });
 
 }
