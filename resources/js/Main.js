ac30a33139a2a83b3c3323e82c86772a3dbdad0b
Juliano Seus <julianoseusdev@outlook.com>
2021-09-22 10:53:36 -0300

    Troca do LurnJs pelo SearchJs

--- resources/js/Main.js
+++ resources/js/Main.js
@@ -13,6 +13,43 @@ static main = function() {
     router.initializePage();
     
     JdsParallax();
+
+    var theGreatGatsby = {
+        isbn: '9781597226769',
+        title: 'The Great Gatsby',
+        author: {
+          name: 'F. Scott Fitzgerald'
+        },
+        tags: ['book', 'inspirational']
+      };
+      var theDaVinciCode = {
+        isbn: '0307474275',
+        title: 'The DaVinci Code',
+        author: {
+          name: 'Dan Brown'
+        },
+        tags: ['book', 'mystery']
+      };
+      var angelsAndDemons = {
+        isbn: '074349346X',
+        title: 'Angels & Demons',
+        author: {
+          name: 'Dan Brown',
+        },
+        tags: ['book', 'mystery']
+      };
+      
+      var search = new JsSearch.Search('isbn');
+      search.addIndex('title');
+      search.addIndex(['author', 'name']);
+      search.addIndex('tags')
+      
+      search.addDocuments([theGreatGatsby, theDaVinciCode, angelsAndDemons]);
+      
+      console.log(search.search('The'));    // [theGreatGatsby, theDaVinciCode]
+      console.log(search.search('scott'));  // [theGreatGatsby]
+      console.log(search.search('dan'));    // [angelsAndDemons, theDaVinciCode]
+      console.log(search.search('mystery')) // [angelsAndDemons, theDaVinciCode]
     /*
     Vendor();
     const pageInformation = new General();
--- resources/js/vendor/vendor.js
+++ resources/js/vendor/vendor.js
@@ -1,7 +1,9 @@
 import Jquery from './../../vendor/Jquery/jquery-3.6.0.min.js';
 import SlickJs from './../../vendor/SlickJs/slick.min.js';
+import JsSearch from './../../vendor/JsSearch/js-search.js';
 
 export default function () { 
     Jquery();
     SlickJs();
+    JsSearch();
 }
